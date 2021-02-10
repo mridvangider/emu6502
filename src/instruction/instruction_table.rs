cf685a511a8bb8e2f00b52870d1a9147c65003d7
Muhammed Ridvan Gider <mridvang@gmail.com>
2021-02-10 12:05:45 +0300

    Added SBC to mnemonics table

--- src/instruction/instruction_table.rs
+++ src/instruction/instruction_table.rs
@@ -4,9 +4,10 @@ use super::util::{
 };
 use super::arithmetic::{
     add,
+    sub
 };
 
-const mnenomics: [Mnemonic;8] = [ 
+const mnenomics: [Mnemonic;16] = [ 
     Mnemonic { name:"ADC", opcode: 0x61, addr_mode: AddressingMode::IndexedIndirect, func: add},
     Mnemonic { name:"ADC", opcode: 0x65, addr_mode: AddressingMode::ZeroPage, func: add },
     Mnemonic { name:"ADC", opcode: 0x69, addr_mode: AddressingMode::Immediate, func: add},
@@ -15,4 +16,12 @@ const mnenomics: [Mnemonic;8] = [
     Mnemonic { name:"ADC", opcode: 0x75, addr_mode: AddressingMode::ZeroPageIndexedX, func: add},
     Mnemonic { name:"ADC", opcode: 0x79, addr_mode: AddressingMode::AbsoluteIndexedY, func: add},
     Mnemonic { name:"ADC", opcode: 0x7D, addr_mode: AddressingMode::AbsoluteIndexedX, func: add},
+    Mnemonic { name:"SBC", opcode: 0xE1, addr_mode: AddressingMode::IndexedIndirect, func: sub},
+    Mnemonic { name:"SBC", opcode: 0xE5, addr_mode: AddressingMode::IndexedIndirect, func: sub},
+    Mnemonic { name:"SBC", opcode: 0xE9, addr_mode: AddressingMode::IndexedIndirect, func: sub},
+    Mnemonic { name:"SBC", opcode: 0xED, addr_mode: AddressingMode::IndexedIndirect, func: sub},
+    Mnemonic { name:"SBC", opcode: 0xF1, addr_mode: AddressingMode::IndexedIndirect, func: sub},
+    Mnemonic { name:"SBC", opcode: 0xF5, addr_mode: AddressingMode::IndexedIndirect, func: sub},
+    Mnemonic { name:"SBC", opcode: 0xF9, addr_mode: AddressingMode::IndexedIndirect, func: sub},
+    Mnemonic { name:"SBC", opcode: 0xFD, addr_mode: AddressingMode::IndexedIndirect, func: sub},
     ];
