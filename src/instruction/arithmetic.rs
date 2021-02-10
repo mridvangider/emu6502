d2a8a636ffb7cae91e38543ba8597aeaa3f2f6d3
Muhammed Ridvan Gider <mridvang@gmail.com>
2021-02-10 12:03:12 +0300

    Implemented SBC

--- src/instruction/arithmetic.rs
+++ src/instruction/arithmetic.rs
@@ -3,12 +3,8 @@ use super::util::{
     Operand,
     Memory,
     calculate_address,
-    FLAG_BREAK,
     FLAG_CARRY,
-    FLAG_DECIMAL,
-    FLAG_INTDIS,
     FLAG_NEGATIVE,
-    FLAG_OVERFLOW,
     FLAG_ZERO,
 
 };
@@ -20,6 +16,7 @@ pub fn add(reg_a: &mut u8, reg_x: &mut u8, reg_y: &mut u8, pc: &mut u16, _sp: &m
                 let old_carry = *stat & FLAG_CARRY;
                 if u8::MAX - *reg_a >= *val {
                     *reg_a += *val;
+                    *stat &= !FLAG_CARRY;
                 } else {
                     *reg_a = (*reg_a).wrapping_add(*val);
                     *stat |= FLAG_CARRY;
@@ -45,7 +42,95 @@ pub fn add(reg_a: &mut u8, reg_x: &mut u8, reg_y: &mut u8, pc: &mut u16, _sp: &m
         AddressingMode::Absolute | AddressingMode::AbsoluteIndexedX | AddressingMode::AbsoluteIndexedY | AddressingMode::ZeroPage | 
         AddressingMode::ZeroPageIndexedX |AddressingMode::IndexedIndirect | AddressingMode::IndirectIndexed => {
             if let Option::Some(addr) = calculate_address(op, reg_x, reg_y, pc, mem, mode) {
-                *reg_a += mem[addr];
+                let val: u8 = mem[addr];
+                let old_carry = *stat & FLAG_CARRY;
+                if u8::MAX - *reg_a >= val {
+                    *reg_a += val;
+                    *stat &= !FLAG_CARRY;
+                } else {
+                    *reg_a = (*reg_a).wrapping_add(val);
+                    *stat |= FLAG_CARRY;
+                }
+
+                if old_carry == FLAG_CARRY {
+                    if u8::MAX != *reg_a {
+                        *reg_a += 1;
+                        *stat &= !FLAG_CARRY;
+                    } else {
+                        *reg_a = (*reg_a).wrapping_add(1);
+                        *stat |= FLAG_CARRY;
+                    }
+                }
+
+                if *reg_a == 0 {
+                    *stat |= FLAG_ZERO;
+                }
+
+                *stat |= FLAG_NEGATIVE & *reg_a;
+            }
+        },
+        _ => {}
+    }
+}
+
+pub fn sub(reg_a: &mut u8, reg_x: &mut u8, reg_y: &mut u8, pc: &mut u16, _sp: &mut u8, stat: &mut u8, op: &Operand, mem: &Memory, mode: AddressingMode) {
+    match mode {
+        AddressingMode::Immediate => {
+            if let Operand::Byte(val) = op {
+                let old_carry = *stat & FLAG_CARRY;
+                if *reg_a >= *val {
+                    *reg_a -= *val;
+                    *stat &= !FLAG_CARRY;
+                } else {
+                    *reg_a = (*reg_a).wrapping_sub(*val);
+                    *stat |= FLAG_CARRY;
+                }
+
+                if old_carry == FLAG_CARRY {
+                    if u8::MIN != *reg_a {
+                        *reg_a -= 1;
+                        *stat &= !FLAG_CARRY;
+                    } else {
+                        *reg_a = (*reg_a).wrapping_sub(1);
+                        *stat |= FLAG_CARRY;
+                    }
+                }
+
+                if *reg_a == 0 {
+                    *stat |= FLAG_ZERO;
+                }
+
+                *stat |= FLAG_NEGATIVE & *reg_a;
+            }
+        },
+        AddressingMode::Absolute | AddressingMode::AbsoluteIndexedX | AddressingMode::AbsoluteIndexedY | AddressingMode::ZeroPage | 
+        AddressingMode::ZeroPageIndexedX |AddressingMode::IndexedIndirect | AddressingMode::IndirectIndexed => {
+            if let Option::Some(addr) = calculate_address(op, reg_x, reg_y, pc, mem, mode) {
+                let val: u8 = mem[addr];
+                let old_carry = *stat & FLAG_CARRY;
+                if *reg_a >= val {
+                    *reg_a -= val;
+                    *stat &= !FLAG_CARRY; 
+                } else {
+                    *reg_a = (*reg_a).wrapping_sub(val);
+                    *stat |= FLAG_CARRY;
+                }
+
+                if old_carry == FLAG_CARRY {
+                    if u8::MIN != *reg_a {
+                        *reg_a -= 1;
+                        *stat &= !FLAG_CARRY;
+                    } else {
+                        *reg_a = (*reg_a).wrapping_sub(1);
+                        *stat |= FLAG_CARRY;
+                    }
+                }
+
+                if *reg_a == 0 {
+                    *stat |= FLAG_ZERO;
+                }
+
+                *stat |= FLAG_NEGATIVE & *reg_a;
             }
         },
         _ => {}
