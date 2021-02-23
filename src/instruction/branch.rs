cddb71ba2a1770a5ac5847b9ee8bd6b5ad3244ab
Muhammed Ridvan Gider <mridvang@gmail.com>
2021-02-23 13:34:27 +0300

    Wrote documentation in branch.rs

--- src/instruction/branch.rs
+++ src/instruction/branch.rs
@@ -1,3 +1,14 @@
+//! # emu6502::instruction::branch
+//! 
+//! This module contains the implementations of the following branch functions:
+//!     - bcc : Branch on carry clear
+//!     - bcs : Branch on carry set
+//!     - bne : Branch on result not zero
+//!     - beq : Branch on result zero
+//!     - bpl : Branch on result plus
+//!     - bmi : Branch on result minus
+//!     - bvc : Branch on overflow clear
+//!     - bvs : Branch on overflow set
 use crate::util::{
     AddressingMode,
     Operand,
@@ -10,6 +21,9 @@ use crate::util::{
 use crate::cpu::*;
 
 impl CPU {
+    /// Branch on carry clear
+    /// 
+    /// branch on C = 0
     pub fn bcc(&mut self, op : &Operand, mode: AddressingMode) {
         if self.stat & FLAG_CARRY == 0 {
             if mode == AddressingMode::Relative {
@@ -20,6 +34,9 @@ impl CPU {
         }
     }
 
+    /// Branch on carry set
+    /// 
+    /// branch on C = 1
     pub fn bcs(&mut self, op : &Operand, mode: AddressingMode) {
         if self.stat & FLAG_CARRY == FLAG_CARRY {
             if mode == AddressingMode::Relative {
@@ -30,6 +47,9 @@ impl CPU {
         }
     }
 
+    /// Branch on result not zero
+    /// 
+    /// branch on Z = 0
     pub fn bne(&mut self, op : &Operand, mode: AddressingMode) {
         if self.stat & FLAG_ZERO == 0 {
             if mode == AddressingMode::Relative {
@@ -40,6 +60,9 @@ impl CPU {
         }
     }
 
+    /// Branch on result zero
+    /// 
+    /// branch on Z = 1
     pub fn beq(&mut self, op : &Operand, mode: AddressingMode) {
         if self.stat & FLAG_ZERO == FLAG_ZERO {
             if mode == AddressingMode::Relative {
@@ -50,6 +73,9 @@ impl CPU {
         }
     }
 
+    /// Branch on result plus
+    /// 
+    /// branch on N = 0
     pub fn bpl(&mut self, op : &Operand, mode: AddressingMode) {
         if self.stat & FLAG_NEGATIVE == 0 {
             if mode == AddressingMode::Relative {
@@ -60,6 +86,9 @@ impl CPU {
         }
     }
 
+    /// Branch on result minus
+    /// 
+    /// branch on N = 1
     pub fn bmi(&mut self, op : &Operand, mode: AddressingMode) {
         if self.stat & FLAG_NEGATIVE == FLAG_NEGATIVE {
             if mode == AddressingMode::Relative {
@@ -70,6 +99,9 @@ impl CPU {
         }
     }
 
+    /// Branch on overflow clear
+    /// 
+    /// branch on V = 0
     pub fn bvc(&mut self, op : &Operand, mode: AddressingMode) {
         if self.stat & FLAG_OVERFLOW == 0 {
             if mode == AddressingMode::Relative {
@@ -80,6 +112,9 @@ impl CPU {
         }
     }
 
+    /// Branch on overflow set
+    /// 
+    /// branch on V = 1
     pub fn bvs(&mut self, op : &Operand, mode: AddressingMode) {
         if self.stat & FLAG_OVERFLOW == FLAG_OVERFLOW {
             if mode == AddressingMode::Relative {
