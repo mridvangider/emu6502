755028848d592febf881766833a518c984574ba5
Muhammed Ridvan Gider <mridvang@gmail.com>
2021-02-24 15:10:27 +0300

    Wrote documentation in setclear.rs

--- src/instruction/setclear.rs
+++ src/instruction/setclear.rs
@@ -3,8 +3,8 @@
 //!     - sec : Set carry
 //!     - cld : Clear decimal
 //!     - sed : Set decimal
-//!     - cli : Clear interrupt
-//!     - sei : Set interrupt
+//!     - cli : Clear interrupt disable
+//!     - sei : Set interrupt disable
 //!     - clv : Clear overflow
 use crate::util::{
     AddressingMode,
@@ -18,42 +18,84 @@ use crate::util::{
 use crate::cpu::*;
 
 impl CPU {
+    /// Clear carry
+    /// 
+    /// 0 -> C
+    /// 
+    /// Effected flags:
+    ///     - Carry
     pub fn clc (&mut self, _op : &Operand, mode: AddressingMode) {
         if mode == AddressingMode::Implied {
             self.stat &= !FLAG_CARRY;
         }
     }
 
+    /// Set carry
+    /// 
+    /// 1 -> C
+    /// 
+    /// Effected flags:
+    ///     - Carry
     pub fn sec (&mut self, _op : &Operand, mode: AddressingMode) {
         if mode == AddressingMode::Implied {
             self.stat |= FLAG_CARRY;
         }
     }
 
+    /// Clear decimal
+    /// 
+    /// 0 -> D
+    /// 
+    /// Effected flags:
+    ///     - Decimal
     pub fn cld (&mut self, _op : &Operand, mode: AddressingMode) {
         if mode == AddressingMode::Implied {
             self.stat &= !FLAG_DECIMAL;
         }
     }
 
+    /// Set decimal
+    /// 
+    /// 1 -> D
+    /// 
+    /// Effected flags:
+    ///     - Decimal
     pub fn sed (&mut self, _op : &Operand, mode: AddressingMode) {
         if mode == AddressingMode::Implied {
             self.stat |= FLAG_DECIMAL;
         }
     }
 
+    /// Clear interrupt disable
+    /// 
+    /// 0 -> I
+    /// 
+    /// Effected flags:
+    ///     - Interrupt Disable
     pub fn cli (&mut self, _op : &Operand, mode: AddressingMode) {
         if mode == AddressingMode::Implied {
             self.stat &= !FLAG_INTDIS;
         }
     }
 
+    /// Set interrupt disable
+    /// 
+    /// 1 -> I
+    /// 
+    /// Effected flags:
+    ///     - Interrupt Disable
     pub fn sei (&mut self, _op : &Operand, mode: AddressingMode) {
         if mode == AddressingMode::Implied {
             self.stat |= FLAG_INTDIS;
         }
     }
 
+    /// Clear overflow
+    /// 
+    /// 0 -> V
+    /// 
+    /// Effected flags:
+    ///     - Overflow
     pub fn clv (&mut self, _op : &Operand, mode: AddressingMode) {
         if mode == AddressingMode::Implied {
             self.stat &= !FLAG_OVERFLOW;
